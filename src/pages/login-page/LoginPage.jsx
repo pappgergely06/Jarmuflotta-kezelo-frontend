db05372b6e3aa7eb8fbf99403dc9e76b1b379164
pappgergely06 <pappgergely2006@gmail.com>
2026-03-19 08:24:00 +0100

    Add LoginPage header and set body width 100% in index.css

--- src/pages/login-page/LoginPage.jsx
+++ src/pages/login-page/LoginPage.jsx
@@ -1,8 +1,15 @@
-import { ChakraProvider, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import style from './LoginPage.module.css'
+import { Provider } from '../../components/ui/provider';
 
 function LoginPage() {
-    return ();
+    return (
+        <Provider>
+            <Flex width={"100%"} alignItems={"center"} flexDirection={"column"} justifyContent={"center"}>
+                <h1>Járműflotta kezelő</h1>
+            </Flex>
+        </Provider>
+    );
 }
 
 export default LoginPage;
