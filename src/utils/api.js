e4f7db24f873e17f7531a6d70502c484eb4b9af7
pappgergely06 <pappgergely2006@gmail.com>
2026-03-25 09:36:53 +0100

    Fix fetchToken async function

--- src/utils/api.js
+++ src/utils/api.js
@@ -25,7 +25,10 @@ export async function fetchUser(token) {
 export async function fetchToken(username, password) {
     try {
 
-        const response = await axios.post(API_URL + "login", username, password)
+        const response = await axios.post(API_URL + "login", {
+            username,
+            password
+        })
 
         return response.data
 
