8fdf75a81e09c658cf135b49dd255bc6c895bc13
pappgergely06 <pappgergely2006@gmail.com>
2026-04-14 17:08:35 +0200

    Try Chakra UI Grid in AdminPage

--- src/pages/admin-page/AdminPage.jsx
+++ src/pages/admin-page/AdminPage.jsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem } from '@chakra-ui/react';
 import { Provider } from '../../components/ui/chakra-snippets/provider';
 import style from './AdminPage.module.css'
 
@@ -6,14 +6,33 @@ function AdminPage() {
     return (
         <Provider>
             <Flex
-                width={"100%"} 
-                minHeight={"90vh"}
+                width={"100%"}
+                minHeight={"100vh"}
                 px={4}
-                alignItems={"center"} 
-                flexDirection={"column"} 
+                alignItems={"center"}
+                flexDirection={"column"}
                 justifyContent={"center"}
+                bg={'rgb(48, 166, 172)'}
             >
-
+                <Grid
+                    h="200px"
+                    templateRows="repeat(2, 1fr)"
+                    templateColumns="repeat(5, 1fr)"
+                    gap={4}
+                >
+                    <GridItem bg={"beige"} rowSpan={2} colSpan={1}>
+                        <Box>rowSpan=2</Box>
+                    </GridItem>
+                    <GridItem bg={"beige"} colSpan={2}>
+                        <Box>colSpan=2</Box>
+                    </GridItem>
+                    <GridItem bg={"beige"} colSpan={2}>
+                        <Box>colSpan=2</Box>
+                    </GridItem>
+                    <GridItem bg={"beige"} colSpan={4}>
+                        <Box>colSpan=4</Box>
+                    </GridItem>
+                </Grid>
             </Flex>
         </Provider>
     );
