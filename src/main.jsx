e259ef48a63f9607b40b9074423dc2900992f105
pappgergely06 <pappgergely2006@gmail.com>
2026-04-14 16:39:06 +0200

    Remove Home Page route from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import LoginPage from './pages/login-page/LoginPage'
 import AdminPage from './pages/admin-page/AdminPage'
-import HomePage from './pages/home-page/HomePage'
 import DriverPage from './pages/driver-page/DriverPage'
 import NotFound from './pages/not-found/NotFound'
 import AuthProvider from './contexts/auth/AuthProvider'
@@ -14,7 +13,6 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='admin' element={<AdminPage />} />
-        <Route path='home' element={<HomePage />} />
         <Route path='driver' element={<DriverPage />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
--- src/pages/admin-page/AdminPage.jsx
+++ src/pages/admin-page/AdminPage.jsx
@@ -1,8 +1,21 @@
+import { Flex } from '@chakra-ui/react';
+import { Provider } from '../../components/ui/chakra-snippets/provider';
 import style from './AdminPage.module.css'
 
 function AdminPage() {
     return (
-        <h1>Admin Page</h1>
+        <Provider>
+            <Flex
+                width={"100%"} 
+                minHeight={"90vh"}
+                px={4}
+                alignItems={"center"} 
+                flexDirection={"column"} 
+                justifyContent={"center"}
+            >
+
+            </Flex>
+        </Provider>
     );
 }
 
