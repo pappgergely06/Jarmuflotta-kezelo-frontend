ff19edcab7ee7be29476d5e218e391f2de17efaa
pappgergely06 <pappgergely2006@gmail.com>
2026-04-15 14:02:34 +0200

    Continue making Overview and Vehicles components

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,9 +1,20 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import GridBox from "../ui/grid-box/GridBox";
 
 function Overview() {
     return (
         <GridBox header={"Flotta áttekintés"}>
-
+            <Flex
+                justifyContent={"center"}
+                alignItems={"center"}
+                flexDirection={"column"}
+                color={"black"}
+            >
+                <Flex padding={"0.5rem 0.5rem 0.5rem 1rem"} width={"100%"} justifyContent={"flex-start"}>
+                    <Text fontWeight={"bold"}>Figyelmeztetések és határidők</Text>
+                </Flex>
+                <Flex></Flex>
+            </Flex>
         </GridBox>
     );
 }
--- src/components/vehicles/Vehicles.jsx
+++ src/components/vehicles/Vehicles.jsx
@@ -1,9 +1,21 @@
+import { Button, Flex, Text } from "@chakra-ui/react";
 import GridBox from "../ui/grid-box/GridBox";
 
 function Vehicles() {
     return (
         <GridBox header={"Járművek"}>
-
+            <Flex 
+                justifyContent={"flex-end"}
+                padding={"0.5rem"}
+            >
+                <Button
+                    w={"20%"}
+                    h={"2rem"}
+                    color={"white"}
+                >
+                    <Text fontSize={"sm"}>Jármű hozzáadása</Text>
+                </Button>
+            </Flex>
         </GridBox>
     );
 }
