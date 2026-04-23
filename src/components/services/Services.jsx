6259c4406f910d83ef8237fdd6ec953cd3b42641
pappgergely06 <pappgergely2006@gmail.com>
2026-04-23 20:29:14 +0200

    Continue Services component

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,14 @@
+import { VStack } from "@chakra-ui/react";
+import GridBox from "../ui/grid-box/GridBox";
+
+function Services() {
+    return (
+        <GridBox header={"Tankolások"}>
+            <VStack h="calc(100% - 5vh)">
+                
+            </VStack>
+        </GridBox>
+    );
+}
+
+export default Services;
