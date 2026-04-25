2e228f1b23c75bfa64aedffc198135244c713fa9
pappgergely06 <pappgergely2006@gmail.com>
2026-04-25 16:12:25 +0200

    Implement saving service

--- src/components/data-input/forms/service-form/ServiceForm.jsx
+++ src/components/data-input/forms/service-form/ServiceForm.jsx
@@ -1,64 +1,63 @@
-import { Flex, HStack, IconButton, Text, VStack } from "@chakra-ui/react";
+import { Alert, Box, Flex, HStack, IconButton, Text, VStack } from "@chakra-ui/react";
 import "../../../forms-style/FormsStyle.module.css";
 import { useState } from "react";
 import { FaScrewdriverWrench } from "react-icons/fa6";
 import { IoIosSave } from "react-icons/io";
+import useAuth from "../../../../hooks/useAuth";
+import useSelectedVehicle from "../../../../hooks/useSelectedVehicle";
 import { addService } from "../../../../utils/api";
 import Cookies from "js-cookie";
 import { useRefresh } from "../../../../contexts/refresh/RefreshContext";
-import useSelectedVehicle from "../../../../hooks/useSelectedVehicle";
-import useAuth from "../../../../hooks/useAuth";
-
 
 function ServiceForm({ width }) {
+    const { triggerRefresh } = useRefresh();
+    const { user } = useAuth();
+    const { selectedVehicle } = useSelectedVehicle();
 
     const today = new Date().toISOString().split('T')[0];
 
     const [date, setDate] = useState(today);
     const [type, setType] = useState("");
     const [nextService, setNextService] = useState(today);
-    const [title, setTitle] = useState("");
-    const [price, setPrice] = useState(0);
-    const [errors, setErrors] = useState({});
-
-    const { triggerRefresh } = useRefresh();
-    const { user } = useAuth();
-    const { selectedVehicle } = useSelectedVehicle();
-
-    const vehicle_id = user?.role === "admin" ? selectedVehicle : user?.driver_vehicle_id;
-
-    const validate = () => {
-        let e = {};
-        if (!date) e.date = "Dátum kötelező";
-        if (!title) e.title = "Megnevezés kötelező";
-        if (!type) e.type = "Típus kötelező";
-        if (price < 1) e.price = "Hibás ár";
-        setErrors(e);
-        return Object.keys(e).length === 0;
-    };
+    const [error, setError] = useState(null);
 
     function clearForm() {
         setDate(today);
         setType("");
         setNextService(today);
+        setError(null);
+    }
+
+    function validateService() {
+        if (user?.role === "admin" && selectedVehicle === null) return "Válasszon ki járművet a táblázatból!";
+        if (!date) return "Dátum megadása kötelező!";
+        if (!type || type.trim().length < 3) return "Típus megadása kötelező (min. 3 karakter)!";
+        if (nextService && nextService < date) return "A következő szerviz nem lehet korábban a mostaninál!";
+        return null;
     }
 
     function saveService() {
-        if (!validate()) return;
+        const e = validateService();
+        const vehicle_id = user?.role === "admin" ? selectedVehicle : user?.driver_vehicle_id;
 
-        addService(Cookies.get("auth_token"), {
-            "date": date,
-            "type": type,
-            "next_service": nextService,
-            "vehicle_id": vehicle_id
-        }).then(() => {
-            alert("Szerviz rögzítve!");
-            triggerRefresh("services");
-            clearForm()
-        }).catch(err => {
-            console.error(err);
-            alert("Hiba történt a mentés során.");
-        });
+        if (e === null) {
+            addService(Cookies.get("auth_token"), {
+                "date": date,
+                "type": type,
+                "next_service": nextService,
+                "vehicle_id": vehicle_id
+            })
+            .then(() => {
+                triggerRefresh("services");
+                clearForm();
+            })
+            .catch(err => {
+                console.error(err);
+                setError("Hiba történt a mentés során!");
+            });
+        } else {
+            setError(e);
+        }
     }
 
     return (
@@ -69,28 +68,52 @@ function ServiceForm({ width }) {
             </HStack>
 
             <HStack width={"100%"} alignItems="flex-start">
-                <VStack width={"50%"} align="stretch">
+                <VStack width={"50%"}>
                     <Text color={"black"}>Dátum</Text>
-                    <input min={today} onChange={(e) => setDate(e.target.value)} value={date} type="date" />
-                    {errors.date && <Text color="red.500" fontSize="xs">{errors.date}</Text>}
+                    <input 
+                        onChange={(e) => setDate(e.target.value)} 
+                        value={date} 
+                        type="date" 
+                    />
                 </VStack>
-                <VStack width={"50%"} align="stretch">
+                <VStack width={"50%"}>
                     <Text color={"black"}>Következő szerviz</Text>
-                    <input onChange={(e) => setNextService(e.target.value)} value={nextService} type="date" />
+                    <input 
+                        onChange={(e) => setNextService(e.target.value)} 
+                        value={nextService} 
+                        type="date" 
+                        min={date}
+                    />
                 </VStack>
             </HStack>
 
-            <HStack width={"100%"} alignItems="flex-start">
-                <VStack width={"100%"} align="stretch">
-                    <Text color={"black"}>Típus</Text>
-                    <input onChange={(e) => setType(e.target.value)} value={type} type="text" placeholder="pl. kötelező" />
-                    {errors.type && <Text color="red.500" fontSize="xs">{errors.type}</Text>}
-                </VStack>
-            </HStack>
+            <VStack width={"100%"}>
+                <Text color={"black"}>Típus / Megnevezés</Text>
+                <input 
+                    style={{ width: "100%" }}
+                    onChange={(e) => setType(e.target.value)} 
+                    value={type} 
+                    type="text" 
+                    placeholder="pl. Olajcsere, Műszaki vizsga"
+                />
+            </VStack>
 
-            <IconButton onClick={saveService} bg={"green.600"} color="white" mt={2}>
-                <IoIosSave />
-                <Text ml={2}>Rögzít</Text>
+            {error !== null && (
+                <Box>
+                    <Alert.Root status="error">
+                        <Alert.Indicator />
+                        <Alert.Title>
+                            {error}
+                        </Alert.Title>
+                    </Alert.Root>
+                </Box>
+            )}
+
+            <IconButton onClick={saveService} bg={"green.600"} color="white" mt={"auto"}>
+                <HStack spacing={2}>
+                    <IoIosSave />
+                    <Text>Rögzít</Text>
+                </HStack>
             </IconButton>
         </Flex>
     );
